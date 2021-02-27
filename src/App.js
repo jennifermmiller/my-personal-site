a3f29f7172a0c0333ff1c037d90dd6fe6efe5238
Jennifer Miller <jmiller6128@gmail.com>
2021-02-27 13:07:40 -0500

    adding routes; misc updates

--- src/App.js
+++ src/App.js
@@ -1,25 +1,65 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react'
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+import { ThemeProvider } from 'styled-components'
 
+import data from './constants/data'
+import Header from './components/Header'
+import Footer from './components/Footer'
+import About from './pages/About'
+import Chuck from './pages/Chuck'
+import Contact from './pages/Contact'
+import Home from './pages/Home'
+import Work from './pages/Work/Work'
+import { GlobalStyles } from './theme/GlobalStyles'
+import { lightTheme, darkTheme } from './theme/themes'
+
+//TODO:
+//  Flush out work page
+//  Fix prettier -- doesn't seem to be hitting all files
+//  add Typescript
+//  add tests? not a ton of functionality
+//  Longterm: put data on server???
 function App() {
+  const { about, chuck, contact, footer, home, navigation, personalLinks, work } = data
+  const [theme, setTheme] = useState('light')
+
+  const toggleTheme = () =>
+    theme === 'light' ? setTheme('dark') : setTheme('light')
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Router>
+      <ThemeProvider theme={theme === 'light' ? lightTheme : darkTheme}>
+        <div className="App">
+          <GlobalStyles />
+          <Header
+            navigation={navigation}
+            handleThemeClick={toggleTheme}
+            theme={theme}
+          />
+          <main>
+            <Switch>
+              <Route path="/about">
+                <About {...about} />
+              </Route>
+              <Route path="/work-experience">
+                <Work {...work} links={personalLinks}/>
+              </Route>
+              <Route path="/contact">
+                <Contact {...contact} />
+              </Route>
+              <Route path="/chuck">
+                <Chuck {...chuck} />
+              </Route>
+              <Route path="/">
+                <Home {...home} links={personalLinks}/>
+              </Route>
+            </Switch>
+          </main>
+          <Footer {...footer} />
+        </div>
+      </ThemeProvider>
+    </Router>
+  )
 }
 
-export default App;
+export default App
