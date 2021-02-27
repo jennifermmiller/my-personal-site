95f3db3ec150b42c1af08c6d18a991e5845dd532
Jennifer Miller <jmiller6128@gmail.com>
2021-02-27 12:18:01 -0500

    hook for outsideClick

---
+++ src/hooks/useComponentVisible.js
@@ -0,0 +1,41 @@
+import { useCallback, useEffect, useRef } from 'react'
+
+const useComponentVisible = ({ enabled = true, onClickHideComponent, ref }) => {
+  const containerRef = useRef()
+  const targetRef = ref || containerRef
+
+  const handleClickOutside = useCallback(
+    (event) => {
+      if (
+        enabled &&
+        targetRef &&
+        targetRef.current &&
+        !targetRef.current.contains(event.target)
+      ) {
+        console.log('in CB', enabled, targetRef, event.target)
+        onClickHideComponent instanceof Function && onClickHideComponent()
+      }
+    },
+    [enabled, targetRef, onClickHideComponent]
+  )
+
+  useEffect(() => {
+    const handleHideDropdown = (event) => {
+      if (event.key === 'Escape') {
+        onClickHideComponent instanceof Function && onClickHideComponent()
+      }
+    }
+
+    document.addEventListener('keydown', handleHideDropdown, true)
+    document.addEventListener('mousedown', handleClickOutside)
+
+    return () => {
+      document.removeEventListener('keydown', handleHideDropdown, true)
+      document.removeEventListener('mousedown', handleClickOutside)
+    }
+  }, [handleClickOutside, onClickHideComponent])
+
+  return { ref: targetRef }
+}
+
+export default useComponentVisible
